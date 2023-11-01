e0409feff5ded56416e83d1b481bc7930b885dc9
ChilloutGeek <adamgemenez@gmail.com>
2023-11-02 07:44:02 +0800

    customized user model for signup/login

--- drf/drf/settings.py
+++ drf/drf/settings.py
@@ -39,14 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'sensor',
-    'corsheaders'
+    'users'
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = False
-
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
--- drf/drf/urls.py
+++ drf/drf/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/data/', include('sensor.urls'))
+    path('api/data/', include('sensor.urls')),
+    path('api/users/', include('users.urls'))
 ]
